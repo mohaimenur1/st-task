3e30ee4292198232f6a679030a83f90ef39b6088
Mohi Minur <68849665+mohaimenur1@users.noreply.github.com>
2024-05-25 17:35:13 +0600

    🎨: flight info section added

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,23 @@ const Main = () => {
       <div className="pb-[20px] pl-[24px]">
         Home &gt; Booking History &gt; Flight &gt; STFL17121182045413{" "}
       </div>
-      <div className="bg-[#FFFFFF] h-[611px] ml-[24px]">second section</div>
+      <div className="bg-[#FFFFFF] h-[611px] ml-[24px] rounded-[8px]">
+        {/* second section */}
+        <div className="flex justify-between p-[16px]">
+          {/* destination info */}
+          <div className="flex items-center">
+            <div className="mr-[16px] bg-[#1882FF] text-[#FFFFFF] w-8 h-8 rounded-full flex items-center justify-center">
+              1
+            </div>
+            <div>
+              <h5>DAC → JFK</h5>
+              <p>Round Trip . 25 Mar - 4 Apr 2023 . 1 Stop</p>
+            </div>
+          </div>
+          {/* time info */}
+          <div>33h 20m</div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div className="bg-[#FFFFFF] h-[655px] w-[248px]">Sidebar</div>;
+  return (
+    <div className="bg-[#FFFFFF] h-[655px] w-[248px] rounded-[8px]">
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
