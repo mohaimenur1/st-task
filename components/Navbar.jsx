58a489b423badeea848674f4aaf59c6bd503f56d
Mohi Minur <68849665+mohaimenur1@users.noreply.github.com>
2024-05-26 16:23:14 +0600

    💄: add fav icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="h-[68px] bg-[#FFFFFF]">Navbar</div>;
+  return <div className="h-[68px] bg-[#FFFFFF]"></div>;
 };
 
 export default Navbar;
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Sidebar = () => {
-  return (
-    <div className="bg-[#FFFFFF] h-[655px] w-[248px] rounded-[8px]">
-      Sidebar
-    </div>
-  );
+  return <div className="bg-[#FFFFFF] h-[655px] w-[248px] rounded-[8px]"></div>;
 };
 
 export default Sidebar;
